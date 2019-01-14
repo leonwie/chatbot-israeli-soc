6c18b9873d0e5ef0cc1c33417519a57e2b599025
leonwie <leonwie@users.noreply.github.com>
2019-01-14 23:12:42 +0000

    Slimming down code

--- public/chat.js
+++ public/chat.js
@@ -15,23 +15,20 @@ $(function(){
 			socket.emit('new_message', {message : msg});
 		}
 		else {message.val('');}
-	})
+	});
 
 	//Listen on new_message
 	socket.on("new_message_customer", (data) => {
 		message.val('');
 		chatlogs.append("<div class='chat'><div class='user-photo'><img src='images/icon_customer.png' alt=''></div><div class='chat_customer'><p class='message'>" + data.message + "</p></div></div>");
 		chatlogs.scrollTop(chatlogs[0].scrollHeight);
-	})
+	});
 	socket.on("new_message_company", (data) => {
 		message.val('');
-		var x ='';
 		chatlogs.append("<div class='chat'><div class='user-photo'><img src='images/IsraeliSocietyLogo.png' alt=''></div><div class='chat_company'><p class='message'>" + data.message +x+ "</p></div></div>");
 		chatlogs.scrollTop(chatlogs[0].scrollHeight);
-	})
+	});
 });
-
-
 
 var input = document.getElementById("message");
 input.addEventListener("keyup", function(event) {
